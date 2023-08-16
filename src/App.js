29a0564f8b5dafd57de7d701aa9a9a7fa6f9427b
dastageer <dastageer@zendyanmix.com>
2023-08-16 22:02:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import './index.scss'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg xmlns="http://www.w3.org/2000/svg"><path fill="#D6E1FF" fill-rule="evenodd" d="M0 349.974c218.558 116.035 460.05 116.035 724.475 0s502.933-116.035 715.525 0V0H0v349.974z" /></svg>
+      {/* Order Summary
+
+      You can now listen to millions of songs, audiobooks, and podcasts on any
+      device anywhere you like!
+
+      Annual Plan
+      $59.99/year
+
+      Change
+
+      Proceed to Payment
+      Cancel Order */}
+
+      {/* TODO: don't delete */}
+
+      in progress
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+
     </div>
   );
 }
